385c5144d660eeaa0eb2f085df1fe1589d1fec62
Juan Carlos <juanzang.38@gmail.com>
2019-04-24 20:22:07 -0500

    Subiendo proyecto completo de platziConf hawaii a repositorio a github

---
+++ index.js
@@ -0,0 +1,5 @@
+//Esta función es un pluggin de jquery que agrega por nosotros un estilo distinto al tooltip
+//Ya viene includo en las librerias de bootstrap y jquery por defecto
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
